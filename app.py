4ee4ac4ea6a146672c4048ddd14d011baa8bcdd4
nostalgiaCode <nowelywork@gmail.com>
2023-06-01 20:54:11 +0300

    whatsup

---
+++ app.py
@@ -0,0 +1,33 @@
+import os
+
+from flask import Flask
+from flask_restful import Api
+
+from config import UPLOAD_FOLDER, postgresqldocker, postgresqllocal
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = postgresqldocker
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+api = Api(app)
+
+from resources.download import RecordFile
+from resources.newuser import User
+from resources.upload import UploadFile
+
+api.add_resource(User, '/')
+api.add_resource(UploadFile, '/upload')
+api.add_resource(RecordFile, '/record', endpoint='record')
+
+if __name__ == "__main__":
+    with app.app_context():
+        from db import db
+        db.init_app(app)
+        db.create_all()
+        app.run(debug=True, host='0.0.0.0')
+
+        
