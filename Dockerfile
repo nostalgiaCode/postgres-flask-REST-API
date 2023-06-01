8a71be1fa916c0031f26d05a8baae0f81a4e2a85
nostalgiaCode <nowelywork@gmail.com>
2023-06-01 20:48:28 +0300

    whatever

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM python:3.9-alpine
-WORKDIR /app
-COPY . /app
-RUN apk update && apk add postgresql-dev gcc python3-dev musl-dev
-RUN pip install --upgrade pip --user
-RUN pip install -r requirements.txt
-RUN apk add ffmpeg
-EXPOSE 5000
-CMD python ./app.py
--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-folder = os.path.dirname(os.path.abspath(__file__))
-UPLOAD_FOLDER = folder + "/uploads/"
-ALLOWED_EXTENSIONS = {"wav"}
-
-postgresqldocker = "postgresql://postgres:1234@pgsql:5432/"
-postgresqllocal = "postgresql://postgres:1234@localhost:5432/"
